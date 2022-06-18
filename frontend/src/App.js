d6d5abfeb360ad26f1080335f55cc939452604ed
Soham <kayalsoham61@gmail.com>
2022-06-18 20:57:31 +0530

    Modified Home Page and Made Footer Responsive

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
+import Event from "./pages/event";
+import Footer from "./component/Footer";
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/event" component={Event} />
+      </Routes>
+      
+    <Footer />
+    </Router>
+    
+  );
+}
+export default App;
